93b506e7f43e844d06b20fdbdd087c8e6ba692be
Seum Kim <kim@g.ecc.u-tokyo.ac.jp>
2020-06-12 17:40:26 +0900

    font revised

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": "http://localhost:4000/assets/images/blog_profile.JPG"
       },{
         "title": "Top1 Accuracy와 Top5 Accuracy 이해하기",
-        "excerpt":"Top1  Top5  ","categories": ["Blog"],
+        "excerpt":"딥러닝 관련 논문을 읽다보면 Top1, Top5, Top10과 같은 말들을 자주 볼 수 있습니다. 대충 감이 오기는 하지만 정확히 무엇을 의미하는 것일까요? Top1 뒤에는 Accuracy가 오는 경우와 Loss가 오는 경우 두 가지가 있습니다. Top1 Accuracy의 경우에는 가장 가까운 클래스를 예측했을 때 정답이었던 경우의 전체 경우의 수에 대한 비율을 의미합니다. 반대로 Top1...","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/second-post/",
         "teaser": "http://localhost:4000/assets/images/blog_profile.JPG"
