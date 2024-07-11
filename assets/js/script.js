ce2d0fbe9a00442bac50080a33a0465f9c5ff7ec
kimseum <seum@ppk.kr>
2024-07-11 16:28:53 +0900

    Add: three.js background on main page

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById("canvas"),
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x131418);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array(500 * 3);
+
+for (let i = 0; i < vertices.length; i += 3) {
+  const [x, y, z] = randomInSphere(1.5);
+  vertices[i] = x;
+  vertices[i + 1] = y;
+  vertices[i + 2] = z;
+}
+
+geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+
+const material = new THREE.PointsMaterial({
+  color: 0xffffff,
+  size: 0.005,
+  transparent: true,
+});
+
+const points = new THREE.Points(geometry, material);
+scene.add(points);
+
+camera.position.z = 1;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  points.rotation.x -= 0.001;
+  points.rotation.y -= 0.00067;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+function randomInSphere(radius) {
+  let x, y, z, d;
+  do {
+    x = Math.random() * 2 - 1;
+    y = Math.random() * 2 - 1;
+    z = Math.random() * 2 - 1;
+    d = x * x + y * y + z * z;
+  } while (d > 1);
+  const scale = radius / Math.sqrt(d);
+  return [x * scale, y * scale, z * scale];
+}
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
